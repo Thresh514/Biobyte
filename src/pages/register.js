80b6b5c619798e123922082ad4e30b2425408641
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-03-04 12:34:25 -0500

    注册页面send和验证码框对齐

--- src/pages/register.js
+++ src/pages/register.js
@@ -73,81 +73,78 @@ const Register = () => {
   return (
     <div>
       <SimpleHeader />
-    <div className="flex items-center justify-center min-h-screen bg-lightest">
-      <div className="flex flex-col items-center w-full max-w-md p-8 bg-white rounded-lg md:shadow-lg lg:shadow-lg">
-        <h2 className="text-center text-3xl font-semibold mb-6">Create Account</h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div>
-            <label className="block text-sm font-semibold text-gray-700">Email</label>
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-semibold text-gray-700">Password</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-              className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div>
-            <label className="block text-sm font-semibold text-gray-700">Confirm Password</label>
-            <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirm Password"
-              className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-            />
-          </div>
-          <div className="flex items-center space-x-2">
-            {/* 验证码输入框 */}
-            <div className="flex-1">
-              <label className="block text-sm font-semibold text-gray-700">Verification Code</label>
+      <div className="flex items-center justify-center min-h-screen bg-lightest">
+        <div className="flex flex-col items-center w-full max-w-md p-8 bg-white rounded-lg md:shadow-lg lg:shadow-lg">
+          <h2 className="text-center text-3xl font-semibold mb-6">Create Account</h2>
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <div>
+              <label className="block text-sm font-semibold text-gray-700">Email</label>
               <input
-                type="text"
-                name="verificationCode"
-                placeholder="Enter verification code"
-                className="w-[300px] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                value={verificationCode}
-                onChange={(e) => setVerificationCode(e.target.value)}
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
                 required
               />
             </div>
-            {/* 发送验证码按钮 */}
-            {!isCodeSent && (
-              <button
-                type="button"
-                className="px-4 py-2 bg-gray-500 text-white text-lg font-bold rounded-md hover:bg-gray-300 transition duration-300"
-                onClick={handleSendCode}
-              >
-                Send
-              </button>
-            )}
-          </div>
-          
-          <button
-            type="submit"
-            className="w-full px-16 py-3 bg-gray-500 text-white text-white text-lg font-bold rounded-md hover:bg-gray-600 transition duration-300"
-          >
-            Sign Up
-          </button>
-        </form>
+            <div>
+              <label className="block text-sm font-semibold text-gray-700">Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-semibold text-gray-700">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                className="w-[300px] p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div className="flex items-center">
+              <div>
+                <label className="block text-sm font-semibold text-gray-700">Verification Code</label>
+                <input
+                  type="text"
+                  name="verificationCode"
+                  placeholder="Enter verification code"
+                  className="w-[200px] px-4 py-2 border mr-3 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  value={verificationCode}
+                  onChange={(e) => setVerificationCode(e.target.value)}
+                  required
+                />
+                {!isCodeSent && (
+                  <button
+                    type="button"
+                    className="px-6 py-2 bg-gray-400 text-white font-semibold rounded-md hover:bg-gray-500 transition duration-300"
+                    onClick={handleSendCode}
+                  >
+                    Send
+                  </button>
+                )}
+              </div>
+            </div>
+            <button
+              type="submit"
+              className="w-full px-16 py-3 bg-gray-500 text-white text-lg font-bold rounded-md hover:bg-gray-600 transition duration-300"
+            >
+              Sign Up
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
     </div>
   );
 };
