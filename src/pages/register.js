caf9d593d8f67e0d635514629a2fcdc289238117
Kunyu Zheng <kunyuz@bu.edu>
2025-03-03 22:36:04 -0500

    前端发送验证码

--- src/pages/register.js
+++ src/pages/register.js
@@ -8,13 +8,40 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [verificationCode, setVerificationCode] = useState("");
+  const [isCodeSent, setIsCodeSent] = useState(false);
+  
 
   const router = useRouter(); 
+
+  const handleSendCode = async () => {
+    const response = await fetch ("/api/send-verification-code", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email }),
+    });
+
+    const data = await response.json();
+
+    if (response.ok) {
+      alert("Verification code sent to your email.");
+      setIsCodeSent(true);
+    }else{
+      alert(`Failed to send verification code. Please try again. Reason: ${data.message || "Unknown error."}`);
+    }
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (password !== confirmPassword) {
       alert("Passwords do not match");
+      return;
+    }
+
+    if(!verificationCode){
+      alert("Please enter the verification code.");
       return;
     }
 
@@ -24,7 +51,7 @@ const Register = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ email, password }),
+      body: JSON.stringify({ email, password, verificationCode }),
     });
 
     const data = await response.json();
@@ -86,6 +113,32 @@ const Register = () => {
               required
             />
           </div>
+          <div className="flex items-center space-x-2">
+            {/* 验证码输入框 */}
+            <div className="flex-1">
+              <label className="block text-sm font-semibold text-gray-700">Verification Code</label>
+              <input
+                type="text"
+                name="verificationCode"
+                placeholder="Enter verification code"
+                className="w-[300px] px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                value={verificationCode}
+                onChange={(e) => setVerificationCode(e.target.value)}
+                required
+              />
+            </div>
+            {/* 发送验证码按钮 */}
+            {!isCodeSent && (
+              <button
+                type="button"
+                className="px-4 py-2 bg-gray-500 text-white text-lg font-bold rounded-md hover:bg-gray-300 transition duration-300"
+                onClick={handleSendCode}
+              >
+                Send
+              </button>
+            )}
+          </div>
+          
           <button
             type="submit"
             className="w-full px-16 py-3 bg-gray-500 text-white text-white text-lg font-bold rounded-md hover:bg-gray-600 transition duration-300"
