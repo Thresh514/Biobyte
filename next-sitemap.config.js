8096158d7461bc56358898e21001fb270992db15
Thresh514 <165234885+Thresh514@users.noreply.github.com>
2025-04-18 20:03:43 -0400

    add sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: 'https://www.biobyte.shop',
+    generateRobotsTxt: true, // 自动生成 robots.txt
+    changefreq: 'weekly',
+    priority: 0.7,
+    sitemapSize: 5000,
+};  
